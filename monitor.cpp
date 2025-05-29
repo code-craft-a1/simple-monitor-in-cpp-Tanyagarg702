9c687b83ab65acaae22d37ca357433d08fc923a8
Tanyagarg702 <51395223+Tanyagarg702@users.noreply.github.com>
2025-05-29 11:00:11 +0530

    Update monitor.cpp

--- monitor.cpp
+++ monitor.cpp
@@ -1,38 +1,51 @@
 #include "./monitor.h"
-#include <assert.h>
+#include <iostream>
 #include <thread>
 #include <chrono>
-#include <iostream>
+
 using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
 
-int vitalsOk(float temperature, float pulseRate, float spo2) {
-  if (temperature > 102 || temperature < 95) {
-    cout << "Temperature is critical!\n";
+bool isTemperatureOk(float temperature) {
+    return temperature >= 95 && temperature <= 102;
+}
+
+bool isPulseRateOk(float pulseRate) {
+    return pulseRate >= 60 && pulseRate <= 100;
+}
+
+bool isSpO2Ok(float spo2) {
+    return spo2 >= 90;
+}
+
+VitalStatus evaluateVitals(float temperature, float pulseRate, float spo2) {
+    if (!isTemperatureOk(temperature)) return VitalStatus::TemperatureError;
+    if (!isPulseRateOk(pulseRate)) return VitalStatus::PulseError;
+    if (!isSpO2Ok(spo2)) return VitalStatus::SpO2Error;
+    return VitalStatus::OK;
+}
+
+void blinkAlert() {
     for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
+        cout << "\r* " << flush;
+        sleep_for(seconds(1));
+        cout << "\r *" << flush;
+        sleep_for(seconds(1));
     }
-    return 0;
-  } else if (pulseRate < 60 || pulseRate > 100) {
-    cout << "Pulse Rate is out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
+}
+
+void showAlert(VitalStatus status) {
+    switch (status) {
+        case VitalStatus::TemperatureError:
+            cout << "Temperature is critical!\n";
+            break;
+        case VitalStatus::PulseError:
+            cout << "Pulse Rate is out of range!\n";
+            break;
+        case VitalStatus::SpO2Error:
+            cout << "Oxygen Saturation out of range!\n";
+            break;
+        default:
+            return;
     }
-    return 0;
-  } else if (spo2 < 90) {
-    cout << "Oxygen Saturation out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
-    return 0;
-  }
-  return 1;
+    blinkAlert();
 }
