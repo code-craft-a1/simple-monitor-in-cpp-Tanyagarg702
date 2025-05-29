0fff7f256ad82738e1019586ed7e2409e8b07cec
Tanyagarg702 <51395223+Tanyagarg702@users.noreply.github.com>
2025-05-29 11:01:00 +0530

    Update test-monitor.cpp

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -1,7 +1,28 @@
 #include <gtest/gtest.h>
 #include "./monitor.h"
 
-TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
-  ASSERT_FALSE(vitalsOk(99, 102, 70));
-  ASSERT_TRUE(vitalsOk(98.1, 70, 98));
+TEST(Monitor, TemperatureValidation) {
+    EXPECT_FALSE(isTemperatureOk(94.9));
+    EXPECT_FALSE(isTemperatureOk(102.1));
+    EXPECT_TRUE(isTemperatureOk(95.0));
+    EXPECT_TRUE(isTemperatureOk(102.0));
+}
+
+TEST(Monitor, PulseValidation) {
+    EXPECT_FALSE(isPulseRateOk(59));
+    EXPECT_FALSE(isPulseRateOk(101));
+    EXPECT_TRUE(isPulseRateOk(60));
+    EXPECT_TRUE(isPulseRateOk(100));
+}
+
+TEST(Monitor, SpO2Validation) {
+    EXPECT_FALSE(isSpO2Ok(89.9));
+    EXPECT_TRUE(isSpO2Ok(90.0));
+}
+
+TEST(Monitor, VitalsEvaluation) {
+    EXPECT_EQ(evaluateVitals(94, 70, 95), VitalStatus::TemperatureError);
+    EXPECT_EQ(evaluateVitals(98.6, 55, 95), VitalStatus::PulseError);
+    EXPECT_EQ(evaluateVitals(98.6, 75, 85), VitalStatus::SpO2Error);
+    EXPECT_EQ(evaluateVitals(98.6, 75, 95), VitalStatus::OK);
 }
